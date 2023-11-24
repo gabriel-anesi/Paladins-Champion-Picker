d57a874f2c11041c044630bd43e6620f2c37e56e
gabriel-anesi <gabriel.anesi@gmail.com>
2023-11-24 17:00:49 -0300

    Changed option select to checkboxes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,10 @@
-const CLASS_SELECTOR = document.getElementById("class");
+const TK_CHAMPIONS_SELECTOR = document.getElementById("tank");
+const DMG_CHAMPIONS_SELECTOR = document.getElementById("damage");
+const SUP_CHAMPIONS_SELECTOR = document.getElementById("support");
+const FLK_CHAMPIONS_SELECTOR = document.getElementById("flank");
 const SELECT_CHAMP = document.getElementById("campeao");
 const GEN_CHAMP_LISTENER = document.getElementById("gerar_campeao");
 const CHAMP_IMAGE = document.getElementById("champ_image");
-//
 const ALL_CHAMPIONS = ["Androxus", "Ash", "Atlas",
 "Azaan", "Barik", "Betty La Bomba", "Bomb King", "Buck", 
 "Caspian", "Cassie", "Corvus", "Dredge", "Drogoz", "Evie",
@@ -25,25 +27,36 @@ const DMG_CHAMPIONS = ["Betty-La-Bomba", "Bomb-King", "Cassie","Dredge", "Drogoz
 const SUP_CHAMPIONS = ["Corvus", "Furia", "Grohk", "Grover", "Io", "Jenos", "Lillith",
 "Mal'Damba", "Pip", "Rei", "Seris", "Ying"];
 
-const FLANK_CHAMPIONS = ["Androxus", "Buck", "Caspian", "Evie", "Kasumi", "Koga", "Lex", "Maeve",
-"Moji", "Skye", "Talus", "Vatu", "VII", "Vora", "Zhin"];
+const FLK_CHAMPIONS = ["Androxus", "Buck", "Caspian", "Evie", "Kasumi", "Koga", "Lex", 
+"Maeve", "Moji", "Skye", "Talus", "Vatu", "VII", "Vora", "Zhin"];
 
 
-GEN_CHAMP_LISTENER.addEventListener("click", function sortear() {
+GEN_CHAMP_LISTENER.addEventListener("click", function sort() {
     let champSort;
     let champList;
 
-    if (CLASS_SELECTOR.value === "Todas") {
+    if (TK_CHAMPIONS_SELECTOR.checked || DMG_CHAMPIONS_SELECTOR.checked || SUP_CHAMPIONS_SELECTOR.checked || FLK_CHAMPIONS_SELECTOR.checked) {
+        champList = [];
+
+        if (TK_CHAMPIONS_SELECTOR.checked) {
+            champList = champList.concat(TK_CHAMPIONS);
+        }
+
+        if (DMG_CHAMPIONS_SELECTOR.checked) {
+            champList = champList.concat(DMG_CHAMPIONS);
+        }
+
+        if (SUP_CHAMPIONS_SELECTOR.checked) {
+            champList = champList.concat(SUP_CHAMPIONS);
+        }
+
+        if (FLK_CHAMPIONS_SELECTOR.checked) {
+            champList = champList.concat(FLK_CHAMPIONS);
+        }
+    } else {
         champList = ALL_CHAMPIONS;
-    } else if (CLASS_SELECTOR.value === "Tanque") {
-        champList = TK_CHAMPIONS;
-    } else if (CLASS_SELECTOR.value === "Suporte") {
-        champList = SUP_CHAMPIONS;
-    } else if (CLASS_SELECTOR.value === "Dano") {
-        champList = DMG_CHAMPIONS;
-    } else if (CLASS_SELECTOR.value === "Flanco") {
-        champList = FLANK_CHAMPIONS;
     }
+    
     champSort = champList[Math.floor(Math.random() * champList.length)];
     SELECT_CHAMP.innerText = champSort.replace("-", " ");
     SELECT_CHAMP.innerText = champSort.replace(/-/g, " ", "g");
