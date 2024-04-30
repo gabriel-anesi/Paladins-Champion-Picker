b05c2deb64afc5627568f69523eb7e20317095e5
gabriel-anesi <110131801+gabriel-anesi@users.noreply.github.com>
2024-04-30 19:20:51 -0300

    Update script.js
    Kasumi now is a damage champion.

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,14 +20,14 @@ const ALL_CHAMPIONS = ["Androxus", "Ash", "Atlas",
 const TK_CHAMPIONS = ["Ash", "Atlas", "Azaan", "Barik", "Fernando", "Inara", "Khan",
 "Makoa", "Nyx", "Raum", "Ruckus", "Terminus", "Torvald", "Yagorath"];
 
-const DMG_CHAMPIONS = ["Betty-La-Bomba", "Bomb-King", "Cassie","Dredge", "Drogoz",
-"Imani", "Kinessa", "Lian", "Octavia", "Omen", "Saati", "Sha-Lin", "Strix", "Tiberius",
+const DMG_CHAMPIONS = ["Betty-La-Bomba", "Bomb-King", "Cassie", "Dredge", "Drogoz",
+"Imani", "Kasumi", "Kinessa", "Lian", "Octavia", "Omen", "Saati", "Sha-Lin", "Strix", "Tiberius",
 "Tyra", "Viktor", "Vivian", "Willo"];
 
 const SUP_CHAMPIONS = ["Corvus", "Furia", "Grohk", "Grover", "Io", "Jenos", "Lillith",
 "Mal'Damba", "Pip", "Rei", "Seris", "Ying"];
 
-const FLK_CHAMPIONS = ["Androxus", "Buck", "Caspian", "Evie", "Kasumi", "Koga", "Lex", 
+const FLK_CHAMPIONS = ["Androxus", "Buck", "Caspian", "Evie", "Koga", "Lex", 
 "Maeve", "Moji", "Skye", "Talus", "Vatu", "VII", "Vora", "Zhin"];
 
 
